e1c35e123ebe2919d0d18ae80c205f6f0e0689a1
Chris Cheetham <ccheetham@gmail.com>
2011-12-09 16:13:09 -0500

    updated vim setup

--- vim/vim/setup.sh
+++ vim/vim/setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname $0)
-MKDIR='mkdir -p'
-LINK='ln -sf'
+MKDIR='/bin/mkdir -p'
+LINK='/bin/ln -sf'
+LS='/bin/ls'
 
 AUTOLOAD_DIR=$BASEDIR/autoload
 PATHOGEN=${PATHOGEN:-~/src/vim-pathogen}
 BUNDLE_DIR=$BASEDIR/bundle
-SOLARIZED_BUNDLE=${SOLARIZED_BUNDLE:-~/src/solarized/vim-colors-solarized}
+BUNDLES="solarized/vim-colors-solarized vim-easymotion vim-surround snipmate.vim tcomment_vim nerdtree vim-fugitive"
 
 function die() {
   echo $* >&2
@@ -21,13 +22,29 @@ function safe_mkdir() {
   $MKDIR $path
 }
 
-echo setting up pathogen
+function add_plugin() {
+  local srcdir=$1
+  [[ -d $srcdir ]] || return
+  subdirs=$(find $srcdir -type d -depth 1 -name '[A-z]*')
+  for d in $subdirs; do
+    for f in $($LS $d); do
+      $LINK $d/$f $BASEDIR/$(basename $d)
+    done
+  done
+}
+
+
+echo -n setting up pathogen...
 safe_mkdir $AUTOLOAD_DIR
 $LINK $PATHOGEN/autoload/pathogen.vim $AUTOLOAD_DIR/
+echo done
 
-echo setting up solarized
-$MKDIR $BASEDIR/bundle
-safe_mkdir $BUNDLE_DIR
-$LINK $SOLARIZED_BUNDLE $BUNDLE_DIR/
+echo -n setting up bundles...
+for bundle in $BUNDLES; do
+  $LINK ~/src/$bundle $BUNDLE_DIR/
+done
+echo done
 
+echo -n setting up git-vim...
+add_plugin ~/src/git-vim
 echo done
